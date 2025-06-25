fea76fb5843e0809025d2695af989dc6c057af8b
dinahnuraini <112395290+dinahnuraini1@users.noreply.github.com>
2025-06-26 00:02:22 +0700

    Update panen_padi.py

--- panen_padi.py
+++ panen_padi.py
@@ -475,7 +475,7 @@ def main():
                         mape_train = model_data.get("mape_train")
                         mape_test = model_data.get("mape_test")
 
-                        if model_rf_pso and mape_train is not None and mape_test is not None:
+                         if model_rf_pso is not None and isinstance(params, dict) and isinstance(mape_train, (float, int)) and isinstance(mape_test, (float, int)):
                             st.subheader("📌 Parameter PSO")
                             st.markdown(f"**Partikel : 100**")
                             st.markdown(f"**Iterasi: 50**")
